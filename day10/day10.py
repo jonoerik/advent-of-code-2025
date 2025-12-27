058c3252c10650d65c8639d60c204a234cb93f96
Jonathan Franzi <jonoerik@fastmail.fm>
2025-12-27 22:17:54 +1100

    Complete day10 part2.

--- day10/day10.py
+++ day10/day10.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-import collections.abc
-import heapq
 import itertools
-import numbers
 from pathlib import Path
+import pulp
 import re
-import typing
 
 
 class Machine:
@@ -67,87 +64,20 @@ def part1(input_data: InputType) -> ResultType:
 
 
 def part2(input_data: InputType) -> ResultType:
-    def a_star_admissible(f):
-        """Mark heuristic function f as admissible as an A* heuristic."""
-        f.is_a_star_admissible = True
-        return f
-
-    def a_star_consistent(f):
-        """Mark heuristic function f as a consistent A* heuristic."""
-        f.is_a_star_consistent = True
-        return f
-
-    def a_star_solve[
-            T, C: numbers.Real
-            ](
-            start: T, goal: T,
-            get_next_nodes: typing.Callable[[T], typing.Iterable[tuple[T, C]]],
-            heuristic: typing.Callable[[T], C],
-            ) -> C:
-        """
-        Solve a search problem using the A* algorithm, finding the cost to reach the goal.
-
-        :param T: Type of nodes in the search graph.
-        :param C: Type of edge cost. Probably int or float.
-        :param start: Start node.
-        :param goal: Target end node.
-        :param get_next_nodes: Function taking a node, and returning an iterable that gives (next-node, cost) pairs.
-        :param heuristic: Function taking a node, and returning the heuristic estimate of the cost from that node to the goal.
-        :return: Best cost from start to goal.
-        """
-        assert getattr(heuristic, "is_a_star_admissible", False), "A* requries an admissible heuristic."
-
-        # Set of nodes yet to be searched, as:
-        # * Estimated total cost to goal (cost to reach + estimated cost to goal)
-        # * Cost to reach this node from start
-        # * Node
-        open_set: list[tuple[C, C, T]] = [(heuristic(start), 0, start)]
-
-        if getattr(heuristic, "is_a_star_consistent", False):
-            visited: set[T] = {start}
-
-            while open_set:
-                _, current_cost, current_node = heapq.heappop(open_set)
-                if current_node == goal:
-                    return current_cost
-
-                for next_node, next_cost in get_next_nodes(current_node):
-                    tentative_cost = current_cost + next_cost
-                    if next_node not in visited:
-                        heapq.heappush(open_set, (tentative_cost + heuristic(next_node), tentative_cost, next_node))
-                        visited.add(next_node)
-        else:
-            # Dictionary of node already visited, to the cost to reach that node from start.
-            visited: dict[T, C] = {start: 0}
-
-            while open_set:
-                _, current_cost, current_node = heapq.heappop(open_set)
-                if current_node == goal:
-                    return current_cost
-
-                for next_node, next_cost in get_next_nodes(current_node):
-                    tentative_cost = current_cost + next_cost
-                    if next_node not in visited or tentative_cost < visited[next_node]:
-                        heapq.heappush(open_set, (tentative_cost + heuristic(next_node), tentative_cost, next_node))
-                        visited[next_node] = tentative_cost
-
-        raise RuntimeError("No path from start node to goal found")
-
-
     def min_button_presses(machine: Machine) -> int:
-        def get_next_nodes(n: tuple[int, ...]) -> collections.abc.Generator[tuple[tuple[int, ...], int]]:
-            for button in machine.buttons:
-                next_node = tuple(x + 1 if i in button else x for i, x in enumerate(n))
-                if all(next_val <= goal_val for next_val, goal_val in zip(next_node, machine.joltages, strict=True)):
-                    yield next_node, 1
+        # This is an integer programming problem.
+        # Variables are the number of times each button is pressed.
+        # Goal is to minimise sum of button variables.
+        # We have 1 constraint per joltage.
 
-        @a_star_admissible
-        @a_star_consistent
-        def heuristic(n: tuple[int, ...]) -> int:
-            #TODO explain heuristic, and why it's admissible and consistent.
-            assert all(x <= g for x, g in zip(n, machine.joltages, strict=True))
-            return max(g - x for x, g in zip(n, machine.joltages, strict=True))
-
-        return a_star_solve((0,) * len(machine.joltages), machine.joltages, get_next_nodes, heuristic)
+        problem = pulp.LpProblem(sense=pulp.LpMinimize)
+        button_vars = [pulp.LpVariable(name=f"button_{i}", lowBound=0, cat="Integer")
+                       for i in range(len(machine.buttons))]
+        problem += sum(button_vars)
+        for i, j in enumerate(machine.joltages):
+            problem += sum(bv for bv, b in zip(button_vars, machine.buttons, strict=True) if i in b) == j
+        problem.solve(pulp.PULP_CBC_CMD(msg=False))
+        assert pulp.LpStatus[problem.status] == "Optimal"
+        return int(sum(v.varValue for v in problem.variables()))
 
     return sum(min_button_presses(m) for m in input_data)
